c0541475ae3a33d5115b94118056635fbe7c3754
IndrajeetNinjaCoder <2003indrajeet@gmail.com>
2024-12-04 17:12:58 +0530

    first commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by test on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
